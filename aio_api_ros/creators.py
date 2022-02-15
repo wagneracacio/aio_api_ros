2f33f1a5ad893824d545bcf385183fedc16dc00f
wagneracacio <wmaluk@gmail.com>
2022-02-15 16:27:24 -0300

    Update creators.py

--- aio_api_ros/creators.py
+++ aio_api_ros/creators.py
@@ -3,9 +3,9 @@ from .simple_pool import ApiRosSimplePool
 
 
 async def create_rosapi_connection(mk_ip: str, mk_port: int, mk_user: str,
-                                   mk_psw: str, loop=None):
+                                   mk_psw: str, loop=None, ssl=False):
     connection = ApiRosConnection(mk_ip=mk_ip, mk_port=mk_port,
-                                  mk_user=mk_user, mk_psw=mk_psw, loop=loop)
+                                  mk_user=mk_user, mk_psw=mk_psw, loop=loop, ssl=ssl)
 
     await connection.connect()
     await connection.login()
@@ -16,5 +16,5 @@ async def create_rosapi_simple_pool(mk_ip: str, mk_port: int, mk_user: str,
                                     mk_psw: str, max_size: int, loop=None):
     simple_pool = await ApiRosSimplePool(mk_ip=mk_ip, mk_port=mk_port,
                                          mk_user=mk_user, mk_psw=mk_psw,
-                                         max_size=max_size, loop=loop)
+                                         max_size=max_size, loop=loop, ssl=ssl)
     return simple_pool
